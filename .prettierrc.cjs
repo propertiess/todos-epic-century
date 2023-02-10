ec377953e56cb2b4f6a71874fc96e9eb47ef1d11
propertiess <erofeev2001@yandex.ru>
2023-02-10 15:42:16 +0300

    chore: update prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,19 +1,3 @@
 module.exports = {
-  tabWidth: 2,
-  arrowParens: 'avoid',
-  semi: true,
-  trailingComma: 'none',
-  singleQuote: true,
-  jsxSingleQuote: true,
-  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
-  importOrder: [
-    '^(react|next(.*))$',
-    '<THIRD_PARTY_MODULES>',
-    '^@/components$',
-    '^@/components/(.*)$',
-    '^@/(.*)$',
-    '^[./]',
-    '^(.*)(sass|css|scss)$'
-  ],
-  importOrderSortSpecifiers: true
+  ...require('@propetiess/prettier-config')
 };
